10d67fcf88c193e8c45556bd0fae9963d2dbc0e9
Michel Atieno <mishqamish@gmail.com>
2019-02-18 14:59:17 +0300

    Refactored code to make it more usable

--- tweepy_streamer.py
+++ tweepy_streamer.py
@@ -4,21 +4,46 @@ from tweepy import Stream
 
 import twitter_credentials
 
-class StdOutListener(StreamListener):
+class TwitterStreamer():
+    '''
+    Class for streaming and processing live tweets.
+    '''
+    def __init__(self):
+        pass
+    def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
+        #This handles Twitter Authentication and the connection to the Twitter Streaming API
+        listener = StdOutListener(fetched_tweets_filename)
+        auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
+        auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
 
-    def on_data(self, data):
-        print(data)
-        return True
+        stream = Stream(auth, listener)
+
+        stream.filter(track =hash_tag_list)
+
+class StdOutListener(StreamListener):
+    '''
+    Basic listener class rhat prints received tweets to stdout
+    '''
+    def __init__(self, fetched_tweets_filename):
+        self.fetched_tweets_filename = fetched_tweets_filename
     
+    def on_data(self, data):
+        try:
+            print(data)
+            with open(self.fetched_tweets_filename, 'a') as tf:
+                tf.write(data)
+            return True
+        except BaseException as e:
+            print("Error on_data: %s" % str(e))
+        return True
     def on_error(self, status):
         print(status)
 
 if __name__ == "__main__":
+     
+     hash_tag_list = ['kenya']
+     fetched_tweets_filename = "tweets.json"
 
-    listener = StdOutListener()
-    auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
-    auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
-
-    stream = Stream(auth, listener)
-
-    stream.filter(track = ['kenya'])
+     twitter_streamer = TwitterStreamer()
+     twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
+   
