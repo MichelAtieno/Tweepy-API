d134155d6e32f844445e60613faaa8988e24496e
Michel Atieno <mishqamish@gmail.com>
2019-02-19 15:19:29 +0300

    Plotted more than one analysis on graph

--- tweepy_streamer.py
+++ tweepy_streamer.py
@@ -130,9 +130,20 @@ if __name__ == "__main__":
     # time_likes.plot(figsize=(16, 4), color='r')
     # plt.show()
 
+    # time_retweets = pd.Series(data=df['retweets'].values, index=df['date'])
+    # time_retweets.plot(figsize=(16, 4), color='r')
+    # plt.show()
+
+    time_likes = pd.Series(data=df['likes'].values, index=df['date'])
+    time_likes.plot(figsize=(16, 4), label="likes", legend=True)
+    
     time_retweets = pd.Series(data=df['retweets'].values, index=df['date'])
-    time_retweets.plot(figsize=(16, 4), color='r')
+    time_retweets.plot(figsize=(16, 4), label="retweets", legend=True)
+    
     plt.show()
+    
+
+    
     # print(dir(tweets[0]))
     # print(tweets[8].retweet_count)
     
